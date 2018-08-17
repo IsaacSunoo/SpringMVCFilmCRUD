70048380ce2bb44a95e87494da3913be545704e2
Isaac <Isaacsunoo@gmail.com>
2018-08-17 17:23:51 -0600

    FilmDAO interface update

--- src/main/java/com/skilldistillery/film/entities/FilmDAO.java
+++ src/main/java/com/skilldistillery/film/entities/FilmDAO.java
@@ -1,5 +1,12 @@
 package com.skilldistillery.film.entities;
 
 public interface FilmDAO {
-
+	public Film getFilmbyFilmId();
+	public Film addNewFilm();
+	public Film deleteFilm();
+	public Film editFilm();
+	// retrieve film and have the option of editing it
+	
+	
+	
 }
