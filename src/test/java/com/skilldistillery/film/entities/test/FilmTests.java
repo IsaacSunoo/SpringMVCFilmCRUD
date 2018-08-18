cda4903112a3c67a7bc054cfd6d6ce7d537e5aee
Stephanie <samcatee@gmail.com>
2018-08-18 11:14:34 -0600

    Initial IMPL of CSS and HTML sheets.

--- src/test/java/com/skilldistillery/film/entities/test/FilmTests.java
+++ src/test/java/com/skilldistillery/film/entities/test/FilmTests.java
@@ -6,6 +6,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.skilldistillery.film.entities.Film;
+
 public class FilmTests {
 	//comment
 	@Before
@@ -15,10 +17,41 @@ public class FilmTests {
 	@After
 	public void tearDown() throws Exception {
 	}
-
+	
+	@Before
 	@Test
 	public void test() {
 		fail("Not yet implemented");
 	}
-
+	@After
+	
+	@Before
+	@Test
+	public Film getFilmbyFilmId() {
+		
+	}
+	@After
+	
+	@Before
+	@Test
+	public Film addNewFilm() {
+		
+	}
+	@After
+	
+	@Before
+	@Test
+	public Film deleteFilm() {
+		
+	}
+	@After
+	
+	@Before
+	@Test
+	public Film editFilm() {
+		
+	}
+	@After
 }
+}
+
